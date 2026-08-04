d4d2c6c4d72c033758c8ca5946ed9b2a3725ace8
snyderphina-com <snyderphina@gmail.com>
2026-08-04 12:59:54 +0300

    fix better auth cross site cookie settings

--- BACKEND/src/db/lib/auth.ts
+++ BACKEND/src/db/lib/auth.ts
@@ -1,37 +1,45 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../index.js"; 
+import { db } from "../index.js";
 import * as schema from "../schema/auth.js";
 
 export const auth = betterAuth({
-    secret: process.env.BETTER_AUTH_SECRET!,             
-    trustedOrigins: [process.env.FRONTEND_URL!],
-  cookies: {
-    session_token: {
-      attributes: {
-        sameSite: "none",
-        secure: true,
-      },
+  secret: process.env.BETTER_AUTH_SECRET!,
+
+  trustedOrigins: [
+    process.env.FRONTEND_URL!,
+  ],
+
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
     },
   },
 
-    database: drizzleAdapter(db, {
-        provider: "pg", 
-        schema,
-    }),
-    emailAndPassword: {
-        enabled: true,
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+        defaultValue: "student",
+        input: true,
+      },
+
+      imageCldPubId: {
+        type: "string",
+        required: false,
+        input: true,
+      },
     },
-    user: {
-        additionalFields: {
-            role: {
-                 type: 'string', required: false, defaultValue: 'student', input: true
-
-            },
-             imageCldPubId: {
-                 type: 'string', required: false, input: true,
-            },
-        }
-
-    }
+  },
 });
