ad6493381c2dae63a19b96bde39b4a40b601479e
snyderphina-com <snyderphina@gmail.com>
2026-08-06 15:21:06 +0300

    passing schema in db/index.ts

--- BACKEND/src/db/index.ts
+++ BACKEND/src/db/index.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import * as schema from "./schema/index.js";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
 
 const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle(sql);
+export const db = drizzle(sql, {
+  schema,
+});
