c3849bf52da1d04295372594d3ed0457bf2f5add
snyderphina-com <snyderphina@gmail.com>
2026-08-06 13:01:52 +0300

    increase rate limits

--- BACKEND/src/middleware/security.ts
+++ BACKEND/src/middleware/security.ts
@@ -22,18 +22,18 @@ const securityMiddleware = async (
 
     switch (role) {
       case "admin":
-        limit = 20;
-        message = "Admin request limit exceeded (20 per minute). Slow down!";
+        limit = 50;
+        message = "Admin request limit exceeded (50 per minute). Slow down!";
         break;
       case "teacher":
       case "student":
-        limit = 10;
-        message = "User request limit exceeded (10 per minute). Please wait.";
+        limit = 40;
+        message = "User request limit exceeded (40 per minute). Please wait.";
         break;
       default:
-        limit = 5;
+        limit = 30;
         message =
-          "Guest request limit exceeded (5 per minute). Please sign up for higher limits.";
+          "Guest request limit exceeded (30 per minute). Please sign up for higher limits.";
         break;
     }
 
